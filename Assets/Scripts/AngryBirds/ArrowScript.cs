45aba9cedc6ac3e1b673bf257bf229b8811894d5
nebesnayaa <s.nebesnaya5@gmail.com>
2024-11-14 19:22:59 +0200

    Hw2

---
+++ Assets/Scripts/AngryBirds/ArrowScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ArrowScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform rotAnchor;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float dy = Input.GetAxis("Vertical");
+        float angle = this.transform.eulerAngles.z;
+        //if(angle > 180)
+        //{
+        //    angle -= 360;
+        //}
+        //if(-15 < angle + dy && angle + dy < 60)
+        //{
+        //    this.transform.RotateAround(rotAnchor.position, Vector3.forward, dy);
+        //}
+
+        if (angle > 180)
+        {
+            angle -= 360;
+        }
+        if (-50 < angle + dy && angle + dy < 30)
+        {
+            this.transform.RotateAround(rotAnchor.position, Vector3.forward, dy);
+        }
+    }
+}
---
+++ Assets/Scripts/AngryBirds/BirdScript.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class BirdScript : MonoBehaviour
+{
+    [SerializeField]
+    private Transform arrow;
+    private Rigidbody2D rb2d;
+    private bool isLaunched = false;
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && !isLaunched)
+        {
+            rb2d.AddForce(800 * arrow.right);
+            arrow.gameObject.SetActive(false);
+            isLaunched = true;
+        }
+    }
+}
