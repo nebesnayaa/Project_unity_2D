2cab42ef1d15d90622b5bba8cde77c80df4fdeda
nebesnayaa <s.nebesnaya5@gmail.com>
2024-11-14 14:58:27 +0200

    lesson2

--- Assets/Scripts/HexagonScript.cs
+++ Assets/Scripts/HexagonScript.cs
@@ -2,8 +2,18 @@ using UnityEngine;
 
 public class HexagonScript : MonoBehaviour
 {
+    private Rigidbody2D rb2d;
+
     void Start()
     {
-        Debug.Log("Hello world");
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            rb2d.AddForce(Vector2.up * 300);
+        }
     }
 }
